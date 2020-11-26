72b3111424642b5622e80644838bf906d0cd159b
MyungJa <windymork7@naver.com>
2020-11-26 23:39:05 +0900

    inte

---
+++ src/test/java/com/example/demo11111/Demo11111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo11111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo11111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
